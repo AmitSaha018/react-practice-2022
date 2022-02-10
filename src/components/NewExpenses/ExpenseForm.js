723b707717da00187afc2f7452d741eb17cdfc0d
AmitSaha018 <31251262+AmitSaha018@users.noreply.github.com>
2022-02-10 13:24:41 +0530

    section:5 last push after entering chart

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -6,58 +6,23 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-
-  //   const [userInput, setUserInput] = useState({
-  //     enteredTitle: "",
-  //     enteredAmount: "",
-  //     enteredDate: "",
-  //   });
+  
 
   const titleChangeHandler = (e) => {
-    setEnteredTitle(e.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredTitle: e.target.value,
-    // });
-    // setUserInput((prevState) => {
-    //   return {
-    //     ...prevState,
-    //     enteredTitle: e.target.value,
-    //   };
-    // });
+    setEnteredTitle(e.target.value);    
   };
   const amountChangeHandler = (e) => {
-    setEnteredAmount(e.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredAmount: e.target.value,
-    // });
-    // setUserInput((prevState) => {
-    //   return {
-    //     ...prevState,
-    //     enteredAmount: e.target.value,
-    //   };
-    // });
+    setEnteredAmount(e.target.value);    
   };
   const dateChangeHandler = (e) => {
     setEnteredDate(e.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: e.target.value,
-    // });
-    // setUserInput((prevState) => {
-    //   return {
-    //     ...prevState,
-    //     enteredDate: e.target.value,
-    //   };
-    // });
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
     const expenseDate = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseDate);
